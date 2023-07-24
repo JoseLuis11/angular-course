f2cf8f88711d883fe8493640c65df3024eb25758
Jose Luis Castro <jluis.scooter11@hotmail.com>
2023-07-24 00:42:24 -0600

    Uses number pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-    name: string = 'Capitán America'
-    array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    name: string = 'Capitán America';
+    array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    PI = Math.PI;
 }
